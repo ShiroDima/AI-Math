131fd0918a89ddf7d3923d7a41fad7486578dd7a
ShiroDima <sd.akarawak@gmail.com>
2024-04-10 06:44:27 +0100

    UI update for uploaded images

---
+++ frontend/aimath/src/components/Thumbnail.jsx
@@ -0,0 +1,49 @@
+import { XIcon } from "@heroicons/react/solid";
+import axios from "axios";
+import {useUploadedFileContext} from "@/context/UploadedFileContext";
+
+const Thumbnail = ({ imgSrc, fileName }) => {
+    const { uploadedFiles, deleteFile, uploadedFilesCloud, deleteFileCloud } = useUploadedFileContext();
+
+    function deleteUploadedFile() {
+        axios
+            .delete(`http://localhost:8000/image_delete?file_name=${fileName}`)
+            .then((response) => {
+                if (response.status === 200) {
+                    deleteFileCloud(
+                        uploadedFilesCloud.filter(
+                            (file) => file.name !== fileName
+                        )
+                    );
+                } else {
+                    throw new Error(
+                        "An error occurred while trying to delete the image"
+                    );
+                }
+            })
+            .finally(() => {
+                deleteFile(
+                    uploadedFiles.filter((file) => file.name !== fileName)
+                );
+            })
+            .catch((error) => console.log(error));
+    }
+
+    return (
+        <div className={'flex relative w-fit mx-0 h-full max-h-full transition ease-in-out delay-150 hover:opacity-50'}>
+            <img
+                src={imgSrc}
+                alt={'uploaded image'}
+                width={60}
+                height={60}
+                className={'rounded-2xl'}
+            />
+            <XIcon
+                className={"absolute transition bg-contain bg-center ease-in-out delay-150 w-8 h-8 top-3.5 left-3 opacity-0 hover:opacity-100"}
+                onClick={deleteUploadedFile}
+            />
+        </div>
+    )
+}
+
+export default Thumbnail
